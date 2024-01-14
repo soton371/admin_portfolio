307686ad82ecd96cd04fcd8045ed2aec39283d19
Soton <soton371@gmail.com>
2024-01-14 22:09:24 +0600

    text field

--- lib/views/about/about_scr.dart
+++ lib/views/about/about_scr.dart
@@ -1,6 +1,6 @@
-
 import 'package:admin_portfolio/configs/app_colors.dart';
 import 'package:admin_portfolio/configs/app_sizes.dart';
+import 'package:admin_portfolio/utilities/responsive.dart';
 import 'package:admin_portfolio/widgets/app_button.dart';
 import 'package:admin_portfolio/widgets/app_text_field.dart';
 import 'package:flutter/cupertino.dart';
@@ -16,13 +16,11 @@ class AboutScreen extends StatefulWidget {
 }
 
 class _AboutScreenState extends State<AboutScreen> {
-
   final ImagePicker picker = ImagePicker();
   Uint8List? imageBytes;
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: AppColors.scaffoldBg,
       appBar: AppBar(
@@ -33,7 +31,40 @@ class _AboutScreenState extends State<AboutScreen> {
       body: ListView(
         padding: const EdgeInsets.all(AppSizes.bodyPadding),
         children: [
-
+          if (Responsive.isTablet(context) || Responsive.isMobile(context))
+            Card(
+              surfaceTintColor: AppColors.background,
+              elevation: 0.2,
+              child: InkWell(
+                borderRadius: BorderRadius.circular(12),
+                onTap: () async {
+                  final XFile? image =
+                      await picker.pickImage(source: ImageSource.gallery);
+                  if (image != null) {
+                    imageBytes = await image.readAsBytes();
+                    setState(() {});
+                  }
+                },
+                child: Container(
+                  height: 200,
+                  decoration: BoxDecoration(
+                      image: imageBytes == null
+                          ? null
+                          : DecorationImage(
+                              image: MemoryImage(imageBytes!),
+                              fit: BoxFit.cover),
+                      borderRadius: BorderRadius.circular(12)),
+                  child: const Icon(
+                    CupertinoIcons.photo,
+                    color: AppColors.hint,
+                  ),
+                ),
+              ),
+            ),
+          if (Responsive.isTablet(context) || Responsive.isMobile(context))
+            const SizedBox(
+              height: AppSizes.bodyPadding,
+            ),
           Row(
             children: [
               Expanded(
@@ -45,46 +76,55 @@ class _AboutScreenState extends State<AboutScreen> {
                     padding: const EdgeInsets.all(AppSizes.bodyPadding),
                     child: Column(
                       children: [
-                        const AppTextField(label: "Description", hint: "Enter your description",maxLines: 3,),
+                        const AppTextField(
+                          label: "Description",
+                          hint: "Enter your description",
+                          maxLines: 3,
+                        ),
                         const SizedBox(
                           height: AppSizes.bodyPadding,
                         ),
-                        AppTextField(label: "Technologies", hint: "Enter your technology",suffixIcon: IconButton(
-                                  onPressed: (){}, icon: const Icon(Icons.add)),),
+                        AppTextField(
+                          label: "Technologies",
+                          hint: "Enter your technology",
+                          suffixIcon: IconButton(
+                              onPressed: () {}, icon: const Icon(Icons.add)),
+                        ),
                         const SizedBox(
                           height: AppSizes.bodyPadding,
                         ),
-
                         Row(
                           children: [
                             Container(
                                 decoration: BoxDecoration(
                                     color: AppColors.scaffoldBg,
-                                    borderRadius: BorderRadius.circular(4)
-                                ),
-                                padding: const EdgeInsets.symmetric(horizontal: 7, vertical: 2),
+                                    borderRadius: BorderRadius.circular(4)),
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 7, vertical: 2),
                                 child: Row(
                                   children: [
                                     const Text("Dart"),
-                                    const SizedBox(width: 10,),
+                                    const SizedBox(
+                                      width: 10,
+                                    ),
                                     InkWell(
-                                      onTap: (){},
-                                      child: const Icon(CupertinoIcons.multiply_circle_fill,size: 14,color: AppColors.hint,),
+                                      onTap: () {},
+                                      child: const Icon(
+                                        CupertinoIcons.multiply_circle_fill,
+                                        size: 14,
+                                        color: AppColors.hint,
+                                      ),
                                     )
                                   ],
                                 ))
                           ],
                         ),
                         const SizedBox(
-                          height: AppSizes.bodyPadding*3,
+                          height: AppSizes.bodyPadding * 3,
                         ),
-
                         Align(
                           alignment: Alignment.centerRight,
-                          child: AppButton(
-                              name: "Submit",
-                              onPressed: (){}
-                          ),
+                          child: AppButton(name: "Submit", onPressed: () {}),
                         )
                       ],
                     ),
@@ -92,35 +132,42 @@ class _AboutScreenState extends State<AboutScreen> {
                 ),
               ),
 
-              const SizedBox(width: AppSizes.bodyPadding,),
-
-              Expanded(
-                child: Card(
-                  surfaceTintColor: AppColors.background,
-                  elevation: 0.2,
-                  child: InkWell(
-                    borderRadius: BorderRadius.circular(12),
-                    onTap: ()async{
-                      final XFile? image = await picker.pickImage(source: ImageSource.gallery);
-                      if(image != null){
-                        imageBytes = await image.readAsBytes();
-                        setState(() {
-
-                        });
-                      }
-
-                    },
-                    child: Container(
-                      height: 290,
-                      decoration: BoxDecoration(
-                        image:imageBytes== null ? null : DecorationImage(image: MemoryImage(imageBytes!),fit: BoxFit.cover),
-                        borderRadius: BorderRadius.circular(12)
+              if (Responsive.isDesktop(context))
+              const SizedBox(
+                width: AppSizes.bodyPadding,
+              ),
+              if (Responsive.isDesktop(context))
+                Expanded(
+                  child: Card(
+                    surfaceTintColor: AppColors.background,
+                    elevation: 0.2,
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(12),
+                      onTap: () async {
+                        final XFile? image =
+                            await picker.pickImage(source: ImageSource.gallery);
+                        if (image != null) {
+                          imageBytes = await image.readAsBytes();
+                          setState(() {});
+                        }
+                      },
+                      child: Container(
+                        height: 305,
+                        decoration: BoxDecoration(
+                            image: imageBytes == null
+                                ? null
+                                : DecorationImage(
+                                    image: MemoryImage(imageBytes!),
+                                    fit: BoxFit.cover),
+                            borderRadius: BorderRadius.circular(12)),
+                        child: const Icon(
+                          CupertinoIcons.photo,
+                          color: AppColors.hint,
+                        ),
                       ),
-                      child: const Icon(CupertinoIcons.photo,color: AppColors.hint,),
                     ),
                   ),
                 ),
-              ),
             ],
           )
         ],
--- lib/widgets/app_text_field.dart
+++ lib/widgets/app_text_field.dart
@@ -1,6 +1,7 @@
 import 'package:admin_portfolio/configs/app_colors.dart';
 import 'package:admin_portfolio/configs/app_sizes.dart';
 import 'package:admin_portfolio/configs/app_text_style.dart';
+import 'package:admin_portfolio/utilities/responsive.dart';
 import 'package:flutter/material.dart';
 
 class AppTextField extends StatelessWidget {
@@ -22,6 +23,7 @@ class AppTextField extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
+        if(Responsive.isDesktop(context) || Responsive.isTablet(context))
         Row(
           children: [
             Expanded(
@@ -47,16 +49,24 @@ class AppTextField extends StatelessWidget {
                 maxLines: maxLines,
                 decoration: InputDecoration(
                   suffixIcon: suffixIcon,
-                    // suffixIcon: onPressed == null
-                    //     ? null
-                    //     : IconButton(
-                    //         onPressed: onPressed, icon: const Icon(Icons.add)),
                     hintText: hint,
                     border: InputBorder.none),
               ),
             ),
           ],
         ),
+
+        if(Responsive.isMobile(context))
+          TextField(
+            maxLines: maxLines,
+           decoration: InputDecoration(
+                suffixIcon: suffixIcon,
+                hintText: hint,
+                labelText: label,
+            ),
+          ),
+
+        if(Responsive.isDesktop(context) || Responsive.isTablet(context))
         const Divider(
           height: 0,
           color: AppColors.seed,
